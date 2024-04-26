be944668f5fa4e7b277b77639496c7a9fd7246c6
BlurredGirl <163426219+BlurredGirl@users.noreply.github.com>
2024-04-26 16:05:00 +0100

    Removed folder

--- src/LocalStorage.js
+++
@@ -1,6 +0,0 @@
-const [treats, setTreats] = useState(
-    parseInt(localStorage.getItem("treats")) || 0 // parseInt converts the string from local storage to a number
-  );
-  const [tps, setTPS] = useState(
-    parseInt(localStorage.getItem("treatsPerSecond")) || 1
-  );
